97139c7c09f98027794bd71a532f4bd550de6875
José Gramajo <gra22907@uvg.edu.gt>
2024-05-18 15:38:43 -0600

    Design Component AboutMe

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import styles from './AboutMe.module.css'
+import Animacion from '../Animation/Animation.jsx' 
+import perfil from '../../assets/perfil1.jpg'
 
 const AboutMe = () => {
     return (
-        <div>
-            <h1>Sobre Mi</h1>
-            <p>Breve descripción sobre mí.</p>
-        </div>
-    );
-};
+        <section className={styles.AboutMe}>
+            <Animacion/>
+           <article className={styles.intro}>
+                <div className={styles.img}>
+                    <img src={perfil} alt="" />
+                </div>
+                <p>Hola, mi nombre es José Luis Jr. Gramajo Moraga. Soy apasionado por la tecnología y el aprendizaje continuo. Desde joven, la tecnología ha sido mi herramienta para mejorar el mundo. Me considero curioso y siempre busco nuevos conocimientos. Además, valoro la disciplina y la perseverancia, cualidades que he cultivado en mi vida profesional y personal. Creo en la importancia de adaptarse a los cambios y enfrentar desafíos con una actitud positiva y proactiva.</p>
+                <div className={styles.adornos}>
+                <div></div>
+                <div></div>
+                <div></div>
+                </div>
+            </article>
 
-export default AboutMe; // Exportación por defecto
+            <div className={styles.edu}>
+
+                <h2>Educación</h2>
+
+                <div>
+                    <h3>Bacchillerato en Ciencias y Letras</h3>
+                    <h4>Liceo Guatemala</h4>
+                </div>
+
+                <div>
+                <h3>Ingles Avanzado C1</h3>
+                    <h4>Academia Europea</h4>
+                </div>
+
+                <div>
+                <h3>Ingeniería en CC y TI</h3>
+                    <h4>Universidad del Valle de Guatemala</h4>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default AboutMe;
